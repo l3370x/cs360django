a4b605734e92e45dc900faed6c10103f563bd84b
l3370x <l3370x@1a797066-3eba-4ff6-6f79-0d63e90c9465>
2012-11-28 20:27:21 +0000

    deleted everything

--- mysite/items/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Item(models.Model):
-    text = models.CharField('Text',max_length=200)
-    created = models.DateTimeField('Date Created',auto_now_add=True)
-
--- mysite/items/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('items.views',
-                       url(r'^item/$', 'index'),
-                       url(r'^item/(?P<id>\d+)/$', 'item'),
-                       )
-
